116ae5b4eca63d986ac9032802bf3bd8898c016c
Andrew Kubera <andrew.kubera@cern.ch>
2018-06-18 15:06:11 -0400

    Add cpp14 & cpp17 defaults files

---
+++ defaults-user-root6-prof.sh
@@ -0,0 +1,45 @@
+package: defaults-user-root6-prof
+version: v1
+env:
+  CXXFLAGS: "-fPIC -g -pg -O2 -std=c++11"
+  CFLAGS: "-fPIC -g -pg -O2"
+  CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+disable:
+  - arrow
+  - treelite
+overrides:
+  AliRoot:
+    version: "%(tag_basename)s"
+    tag: v5-09-47b
+    requires:
+      - ROOT
+      - fastjet:(?!.*ppc64)
+      - Vc
+  AliPhysics:
+    version: "%(tag_basename)s"
+    tag: v5-09-47b-01
+  ROOT:
+    tag: "v6-16-00"
+    requires:
+      - AliEn-Runtime:(?!.*ppc64)
+      - GSL
+      - opengl:(?!osx)
+      - Xdevel:(?!osx)
+      - FreeType:(?!osx)
+      - Python-modules
+      - "GCC-Toolchain:(?!osx)"
+  GSL:
+    prefer_system_check: |
+      printf "#include \"gsl/gsl_version.h\"\n#define GSL_V GSL_MAJOR_VERSION * 100 + GSL_MINOR_VERSION\n# if (GSL_V < 116)\n#error \"Cannot use system's gsl. Notice we only support versions from 1.16 (included)\"\n#endif\nint main(){}" | gcc  -I$(brew --prefix gsl)/include -xc++ - -o /dev/null
+---
+# To be used with aliBuild option `--defaults user-root6`.
+#
+# This defaults set is meant to be used on user computers with ROOT 6 as a base.
+#
+#   * Builds AliRoot and AliPhysics without GEANT3, 4 and DPMJET, which fits
+#     most of the use cases and speeds up compilation time dramatically
+#   * Pinpoints AliRoot and AliPhysics to their latest tags, but they will be
+#     overridden by using packages in development mode
+#
+# This defaults set will become the default and will replace "release" at some
+# point.
